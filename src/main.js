c172f9120054bc8faf1055b4622e79233791dbbb
刘圣凯 <949919743@qq.com>
2024-06-21 18:11:44 +0800

    feat：  引入

--- src/main.js
+++ src/main.js
@@ -1,8 +1,18 @@
 import Vue from 'vue'
+import 'xe-utils'
+import VXETable from 'vxe-table'
+import 'vxe-table/lib/style.css'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 import App from './App.vue'
 
 Vue.config.productionTip = false
 
-new Vue({
-  render: h => h(App),
-}).$mount('#app')
+Vue.use(ElementUI)
+Vue.use(VXETable)
+
+const app = new Vue({
+  render: (h) => h(App)
+})
+
+app.$mount('#app')
